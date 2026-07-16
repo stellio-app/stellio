c0c76d0c552789bc42d8aa7731e324030f1bcc81
stellio-app <app.stellio@gmail.com>
2026-07-16 16:36:50 +0200

    Add files via upload

---
+++ install_stellio.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+set -e
+
+echo "============================================"
+echo "  STELLIO - Installation automatique Linux"
+echo "============================================"
+
+echo "[1/6] Mise a jour du systeme..."
+sudo apt update -y && sudo apt upgrade -y
+
+echo "[2/6] Installation des dependances systeme..."
+sudo apt install -y python3 python3-pip python3-venv zip libgtk-3-dev libwebkit2gtk-4.0-dev libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libosmesa6-dev libpython3-dev
+
+echo "[3/6] Creation de l'environnement virtuel..."
+if [  -d "venv" ]; then
+    python3 -m venv venv
+fi
+source venv/bin/activate
+
+echo "[4/6] Installation des dependances Python..."
+pip install --upgrade pip
+pip install pyinstaller flask waitress pywebview numpy pillow matplotlib trimesh pyrender telethon paho-mqtt rarfile py7zr nest-asyncio smbclient cryptography imageio
+
+echo "[5/6] Compilation de l'application..."
+python3 -m PyInstaller --name Stellio --onedir --windowed --noconfirm --clean --icon=assets/logo-nom-stellio.ico --copy-metadata imageio --copy-metadata pyrender --copy-metadata trimesh --copy-metadata Pillow --collect-all numpy --collect-all PIL --collect-all matplotlib --hidden-import flask --hidden-import waitress --hidden-import webview --hidden-import trimesh --hidden-import pyrender --hidden-import numpy --hidden-import PIL --hidden-import matplotlib --hidden-import matplotlib.pyplot --hidden-import mpl_toolkits --hidden-import mpl_toolkits.mplot3d --hidden-import mpl_toolkits.mplot3d.art3d --hidden-import matplotlib.backends --hidden-import matplotlib.backends.backend_agg --hidden-import matplotlib.figure --hidden-import matplotlib.axes --hidden-import smbclient --hidden-import cryptography --collect-all telethon --hidden-import paho.mqtt --hidden-import rarfile --hidden-import py7zr --hidden-import nest_asyncio launcher.py
+
+echo "[6/6] Configuration finale..."
+mkdir -p dist/Stellio/app
+cp main.py dist/Stellio/app/
+cp index.html dist/Stellio/app/
+cp script.js dist/Stellio/app/
+cp style.css dist/Stellio/app/
+cp -r assets dist/Stellio/app/
+cp -r languages dist/Stellio/app/
+cp -r bin dist/Stellio/app/
+
+echo "============================================"
+echo "  Installation terminee avec succes "
+echo "============================================"
+echo "Lancez l'app avec : ./dist/Stellio/Stellio"
